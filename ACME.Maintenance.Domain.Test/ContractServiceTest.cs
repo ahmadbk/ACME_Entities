a38098fb339deaa242a68b1a9427e588a6ba2824
Ahmad Khalid <ahmad@nml.co.za>
2019-02-06 15:00:51 +0200

    Renaming and applying rules to unit tests

---
+++ ACME.Maintenance.Domain.Test/ContractServiceTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ACME.Maintenance.Domain.DTO;
+using ACME.Maintenance.Domain.Interfaces;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACME.Maintenance.Domain.Test
+{
+    [TestClass]
+    public class ContractServiceTest
+    {
+        private IContractRepository _contractRepository;
+        private ContractService _contractService;
+
+        private const string validContractId = "CONTRACTID";
+        private const string expiredContractId = "EXPIREDCONTRACTID";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // All the setup needs to happen here
+            // Even the auto mapping needs to be setup here
+            // Initialize serves as the "composition route"
+
+            _contractRepository = A.Fake<IContractRepository>();
+            _contractService = new ContractService(_contractRepository);
+
+            A.CallTo(() => _contractRepository.GetById(validContractId))
+                .Returns(new ContractDto
+                {
+                    ContractId = validContractId,
+                    ExpirationDate = DateTime.Now.AddDays(1)
+                });
+
+            A.CallTo(() => _contractRepository.GetById(expiredContractId))
+                .Returns(new ContractDto
+                {
+                    ContractId = expiredContractId,
+                    ExpirationDate = DateTime.Now.AddDays(-1)
+                });
+
+            AutoMapper.Mapper.Initialize(cfg => cfg.CreateMap<ContractDto, Contract>());
+
+        }
+
+        //Naming Convention: MethodName_StateUnderTest_ExpectedBehaviour
+        //Pattern to follow in each unit test: Arrange, Act, Assert
+
+        [TestMethod]
+        public void GetById_ValidContractId_ReturnsContract()
+        {
+            //Arrange - All necessary pre-conditions and inputs 
+            
+            //Act - Act on the object or method under test
+            Contract contract = _contractService.GetById(validContractId);
+
+            //Assert - Test for the expected result
+            Assert.IsInstanceOfType(contract, typeof(Contract));
+            Assert.IsTrue(contract.ExpirationDate > DateTime.Now);
+            Assert.AreEqual(validContractId, contract.ContractId);
+        }
+
+        [TestMethod]
+        public void GetById_ExpiredContractId_ReturnsExpiredContract()
+        {
+            Contract contract = _contractService.GetById(expiredContractId);
+
+            Assert.IsInstanceOfType(contract, typeof(Contract));
+            Assert.IsTrue(DateTime.Now > contract.ExpirationDate);
+            Assert.AreEqual(expiredContractId, contract.ContractId);
+        }
+    }
+}
--- ACME.Maintenance.Domain.Test/ContractTest.cs
+++ ACME.Maintenance.Domain.Test/ContractTest.cs
@@ -9,59 +9,6 @@ namespace ACME.Maintenance.Domain.Test
     [TestClass]
     public class ContractTest
     {
-        private IContractRepository _contractRepository;
-
-        private const string validContractId = "CONTRACTID";
-        private const string expiredContractId = "EXPIREDCONTRACTID";
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            // All the setup needs to happen here
-            // Even the auto mapping needs to be setup here
-            // Initialize serves as the "composition route"
-
-            _contractRepository = A.Fake<IContractRepository>();
-
-            A.CallTo(() => _contractRepository.GetById(validContractId))
-                .Returns(new ContractDto
-                {
-                    ContractId = validContractId,
-                    ExpirationDate = DateTime.Now.AddDays(1)
-                });
-
-            A.CallTo(() => _contractRepository.GetById(expiredContractId))
-                .Returns(new ContractDto
-                {
-                    ContractId = expiredContractId,
-                    ExpirationDate = DateTime.Now.AddDays(-1)
-                });
-
-            AutoMapper.Mapper.Initialize(cfg => cfg.CreateMap<ContractDto, Contract>());
-        }
-
-        [TestMethod]
-        public void Contract_ValidContractId_ReturnsContract()
-        {
-            //var contractRepository = new ContractRepository();
-            var contractService = new ContractService(_contractRepository);
-            Contract contract = contractService.GetById(validContractId);
-
-            Assert.IsInstanceOfType(contract, typeof(Contract));
-            Assert.IsTrue(contract.ExpirationDate > DateTime.Now);
-            Assert.AreEqual(validContractId, contract.ContractId);
-        }
-
-        [TestMethod]
-        public void Contract_ExpiredContractId_ReturnsExpiredContract()
-        {
-            //var contractRepository = new ContractRepository();
-            var contractService = new ContractService(_contractRepository);
-            Contract contract = contractService.GetById(expiredContractId);
-
-            Assert.IsInstanceOfType(contract, typeof(Contract));
-            Assert.IsTrue(DateTime.Now > contract.ExpirationDate);
-            Assert.AreEqual(expiredContractId, contract.ContractId);
-        }
+ 
     }
 }
